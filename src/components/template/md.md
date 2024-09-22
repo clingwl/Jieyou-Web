47a2d85a043796938a4dc4cf2c4dabb44b158b64
clingwl <913186941@qq.com>
2024-09-22 11:53:18 +0800

    first commit

---
+++ src/components/template/md.md
@@ -0,0 +1,11 @@
+---
+title:
+description: 
+pubDate:
+image: 
+tags:
+  - 随笔
+categories:
+  - 生活
+---
+conic section
---
+++ src/consts.ts
@@ -0,0 +1,121 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// Site title and description
+export const SITE_TAB = "解忧小栈";
+export const SITE_TITLE = "解忧小栈 ⚡";
+export const SITE_DESCRIPTION = "为君解忧";
+export const DATE_FORMAT = "ddd MMM DD YYYY";
+
+// User profile information
+export const USER_NAME = "子川";
+export const USER_AVATAR = "/profile.webp";
+
+// Server and transition settings
+export const SERVER_URL = "https://waline.jieyou.ltd/";
+export const TRANSITION_API = true;
+
+// Site theme
+export const siteTheme = {
+  light: "winter",
+  dark: "dracula",
+};
+
+// Some informative text on the site
+export const infoTest = {
+  tag: "标签：",
+  noTag: "未分类", //untagged
+  tagCard: "标签",
+  tagPage: "Tag - ",
+  noCategory: "未分类", //uncategorized
+  categoryCard: "分类",
+  categoryPage: "Category - ",
+  link: "链接：",
+  prevPage: "上一页",
+  nextPage: "下一页",
+};
+
+// Menu items for navigation
+export const menuItems = [
+  { id: "home", text: "首页", href: "/", svg: "home", target: "_self" }, // Home page
+  { id: "about", text: "关于", href: "/about", svg: "about", target: "_self" }, // About page
+  {
+    id: "blog",
+    text: "博客",
+    href: "/blog",
+    svg: "blog",
+    target: "_self",
+    subItems: [
+      {
+        id: "all",
+        text: "所有博客",
+        href: "/blog",
+        svg: "post",
+        target: "_self",
+      }, // All blog
+      {
+        id: "tech",
+        text: "技术博客",
+        href: "/blog/categories/tech",
+        svg: "cube",
+        target: "_self",
+      }, // Technology category
+      {
+        id: "life",
+        text: "生活博客",
+        href: "/blog/categories/life",
+        svg: "heart",
+        target: "_self",
+      }, // Life category
+    ],
+  }, // Blog page with sub-items
+  {
+    id: "project",
+    text: "项目",
+    href: "/project",
+    svg: "project",
+    target: "_self",
+  }, // Projects page
+  {
+    id: "friend",
+    text: "朋友",
+    href: "/friend",
+    svg: "friend",
+    target: "_self",
+  }, // Friends page
+  {
+    id: "contact",
+    text: "联系",
+    href: "mailto:contact.evesunmaple@outlook.com", // Contact email
+    target: "_blank", // Open in a new tab
+    svg: "contact",
+  },
+];
+
+// Social media and contact icons
+export const socialIcons = [
+  {
+    href: "https://afdian.net/a/saroprock",
+    ariaLabel: "Support my work",
+    title: "Support my work",
+    svg: "support",
+  },
+  {
+    href: "https://github.com/EveSunMaple",
+    ariaLabel: "Github",
+    title: "Github",
+    svg: "github",
+  },
+  {
+    href: "https://space.bilibili.com/438392347",
+    ariaLabel: "BiliBili",
+    title: "BiliBili",
+    svg: "bilibili",
+  },
+  {
+    href: "/rss.xml",
+    ariaLabel: "RSS Feed",
+    title: "RSS Feed",
+    svg: "rss",
+  },
+];
