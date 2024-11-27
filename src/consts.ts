e9976f75c3af957bcc3ef780346d8e9141ef6514
解忧小栈 <61323191+clingwl@users.noreply.github.com>
2024-11-27 16:48:40 +0800

    123

--- src/consts.ts
+++ src/consts.ts
@@ -2,24 +2,30 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Site title and description
-export const SITE_TAB = "解忧小栈";
-export const SITE_TITLE = "解忧小栈 ⚡";
-export const SITE_DESCRIPTION = "为君解忧";
+export const SITE_LANG = "zh";
+export const SITE_TAB = "SaroProck";
+export const SITE_TITLE = "SaroProck ⚡";
+export const SITE_DESCRIPTION = "一本本子";
 export const DATE_FORMAT = "ddd MMM DD YYYY";
 
 // User profile information
-export const USER_NAME = "子川";
+export const USER_NAME = "EveSunMaple";
+export const USER_SITE = "https://frosti.saroprock.com"
 export const USER_AVATAR = "/profile.webp";
 
 // Server and transition settings
-export const SERVER_URL = "https://waline.jieyou.ltd/";
+export const SERVER_URL = "https://waline.saroprock.com";
 export const TRANSITION_API = true;
 
-// Site theme
-export const siteTheme = {
+// Theme settings
+export const DAISYUI_THEME = {
   light: "winter",
   dark: "dracula",
-};
+}
+export const CODE_THEME = {
+  light: "github-light",
+  dark: "github-dark",
+}
 
 // Some informative text on the site
 export const infoTest = {
@@ -56,17 +62,24 @@ export const menuItems = [
       {
         id: "tech",
         text: "技术博客",
-        href: "/blog/categories/tech",
+        href: "/blog/categories/技术",
         svg: "cube",
         target: "_self",
       }, // Technology category
       {
         id: "life",
         text: "生活博客",
-        href: "/blog/categories/life",
+        href: "/blog/categories/生活",
         svg: "heart",
         target: "_self",
       }, // Life category
+      {
+        id: "categories",
+        text: "所有分类",
+        href: "/blog/categories",
+        svg: "categories",
+        target: "_self",
+      }, // All categories
     ],
   }, // Blog page with sub-items
   {
@@ -95,19 +108,19 @@ export const menuItems = [
 // Social media and contact icons
 export const socialIcons = [
   {
-    href: "https://afdian.net/a/clingwl",
+    href: "https://afdian.net/a/saroprock",
     ariaLabel: "Support my work",
     title: "Support my work",
     svg: "support",
   },
   {
-    href: "https://github.com/clingwl",
+    href: "https://github.com/EveSunMaple",
     ariaLabel: "Github",
     title: "Github",
     svg: "github",
   },
   {
-    href: "https://space.bilibili.com/",
+    href: "https://space.bilibili.com/438392347",
     ariaLabel: "BiliBili",
     title: "BiliBili",
     svg: "bilibili",
@@ -118,4 +131,11 @@ export const socialIcons = [
     title: "RSS Feed",
     svg: "rss",
   },
+];
+// Category Information
+export const categoriesInfo = [
+  { title: "技术", desc: "有关技术的一些文章", target: "_self" },
+  { title: "生活", desc: "生活中的所见所思所想", target: "_self" },
+  { title: "OI学习笔记", desc: "OIer 的“圣遗物”", target: "_self" },
+  { title: "作文", desc: "和随笔有些不同", target: "_self" },
 ];
